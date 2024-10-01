31e8841597150a0a3e62ef4de194c7a57fb4769d
YanetBoLe <yblezcanos@gmail.com>
2024-10-01 10:48:58 +0200

    up code

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+router.get('/', (req, res) => res.render('index', {title : 'First web with node'}));
+router.get('/about', (req, res) => res.render('about', {title : 'About me'}));
+router.get('/contact', (req, res) => res.render('contact', {title : 'Contact page'}));
+
+export default router;
